cc935cfdf245422615c5c54641ef1197930edecc
Tom Savage <tom@crvstech.com>
2022-03-08 18:16:20 +0000

    docs changes

--- docs/make.jl
+++ docs/make.jl
@@ -3,3 +3,6 @@ using Documenter, IntervalAnalysis
 
 
 makedocs(sitename="Interval Analysis",doctest=true,modules=[IntervalAnalysis])
+deploydocs(
+    repo = "github.com/trsav/interval_analysis.git",
+)
